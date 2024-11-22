01d611df8b152a9fd08f9cf71895e0d999c203e0
ZaydenF <zaydenfleming@gmail.com>
2024-11-22 13:43:28 -0800

    finish files

--- static/myscript.js
+++ static/myscript.js
@@ -3,4 +3,4 @@ $(document).ready(function() {
     for (let year = startYear; year <= endYear; year++) {
       document.write('<option value="' + year + '">' + year + '</option>');
     }
-}
+})
--- webapp.py
+++ webapp.py
@@ -7,7 +7,7 @@ import json
 app = Flask(__name__)
 
 @app.route('/')
-def  home():
+def home():
     year = year_option()
     #print(states)
     return render_template('home.html', year_option=year)
@@ -15,10 +15,20 @@ def  home():
 @app.route("/showYear")
 def render_info():
     year = year_option()
-    ayear = request.args.get('ayear')
-    astronaut_count = Astronauts_in_year(ayear)
-    fact = "In the year " + ayear + ", there were " + str(astronaut_count) + " astronauts"
-    return render_template('index2.html', year_option=year, AFact=fact )
+    if "ayear" in request.args:
+        ayear = request.args.get('ayear')
+
+        with open('astronauts.json') as astronaut_data:
+            astronaut = json.load (astronaut_data)
+        count = 0
+        for c in astronaut:
+            if c['Mission']['Year'] == int(ayear):
+                count += 1
+                
+        fact = "In the year " + ayear + ",there were " +str(count) + " astronauts."
+
+        return render_template('index2.html', year_option=year, AFact=fact)
+    return render_template('index2.html', year_option=year)
 
 def year_option():
     with open('astronauts.json') as astronaut_data:
@@ -31,16 +41,21 @@ def year_option():
     for s in year:
         options += Markup("<option value=\"" + str(s) + "\">" + str(s) + "</option>") #Use Markup so <, >, " are not escaped lt, gt, etc.
     return options
-
-def Astronauts_in_year(ayear):
-    with open('astronauts.json') as astronaut_data:
-        years = json.load(astronaut_data)
-    count=0
-    for c in years:
-        if c['Mission']['Year'] == ayear:
-            count += 1
+@app.route('/datanat')
+def render_graph():
+    mission = get_sighted_years()
+    return render_template('index3.html',data = mission)
     
-    return count
+def get_sighted_years():
+    with open('astronauts.json') as astronaut_data:
+        nat = json.load (astronaut_data)
+    mission= "["
+    for n in nat:
+        if n["Profile"]["Nationality"] == "U.S.":
+            mission += Markup("{x:" + str(n["Profile"]["Selection"]["Year"]) + ",y:" + str(n["Mission"]["Durations"]["Mission duration"]) + "}, ")
+    mission = mission[:-1] + "]"
+    return mission
+
     
     
  
@@ -55,7 +70,7 @@ def is_localhost():
 
 
 if __name__ == '__main__':
-    app.run(debug=True) # change to False when running in production
+    app.run(debug=False) # change to False when running in production
 
 #def Astronauts_in_year(ayear):
   #  with open('astronauts.json') as astronaut_data:
